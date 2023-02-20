bef63b86541323ec3569fccff8e71d62f36ace6e
Yana Kukhta <kukhta0school@gmail.com>
2023-02-20 17:38:06 +0300

    create architecture

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,7 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
-    <div className="App">
-    </div>
-  )
+  return <div className="App"></div>
 }
 
 export default App
