bd1aa82500ab3c8bd058cad99037cdfecf952402
Yana Kukhta <kukhta0school@gmail.com>
2023-02-20 18:15:16 +0300

    add font and normalize.css

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  server: {
+    port: 1234,
+    strictPort: false,
+    proxy: {
+      '/auth': {
+        target: 'https://test.gnzs.ru/oauth/get-token.php',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/auth/, ''),
+        headers: {
+          'X-Client-Id': '30878566',
+          'Content-Type': 'application/json'
+        }
+      }
+    }
+  }
 })
