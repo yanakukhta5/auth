68739694b7a84686caf34f35c25b6bef26e6dc10
Yana Kukhta <kukhta0school@gmail.com>
2023-02-23 19:26:54 +0300

    hot fix

--- src/pages/Users/style.tsx
+++ src/pages/Users/style.tsx
@@ -1,24 +1,3 @@
 import styled from '@emotion/styled'
 
-import { Button } from '@/components/Button'
 
-export const UserContainer = styled.div`
-  display: flex;
-`
-
-export const Select = styled.select`
-  margin-bottom: 20px;
-`
-
-export const Userform = styled.form`
-  display: flex;
-  flex-direction: column;
-  width: clamp(300px, 20%, 400px);
-  gap: 30px;
-  margin-bottom: 20px;
-  @media (max-width: 350px) {
-    width: unset;
-  }
-`
-
-export const Submit = Button.withComponent('input')
