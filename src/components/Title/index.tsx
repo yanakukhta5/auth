d4a1ff42890348a514693a880c8c83537533b6be
Yana Kukhta <kukhta0school@gmail.com>
2023-02-23 19:13:20 +0300

    add adaptive Title and Form

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -1,7 +1,10 @@
 import styled from '@emotion/styled'
 
 export const Title = styled.h1`
- text-align: center;
- font-size: 2em;
- margin-bottom: 1em;
+  text-align: center;
+  font-size: 2em;
+  margin-bottom: 1em;
+  @media (max-width: 420px) {
+    font-size: 1.3em;
+  }
 `
