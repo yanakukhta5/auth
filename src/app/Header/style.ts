a7b2fe11e483cd2c5693e898df4026716e55cc0a
Yana Kukhta <kukhta0school@gmail.com>
2023-03-04 14:23:16 +0300

    rename tsx to ts style files

--- src/app/Header/style.ts
+++ src/app/Header/style.ts
@@ -1,9 +1,5 @@
 import styled from '@emotion/styled'
 
-import { secondary } from '@/variables/colors'
-
-import { Row } from '@/components/Row'
-
 export const Header = styled.header`
   width: 100%;
   padding: 20px 0;
--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import styled from '@emotion/styled'
-import { Option } from '@/components/Option'
 
 type SelectProps = {
   options: { name: string; value: string }[]
@@ -16,9 +15,9 @@ export const Select: React.FC<SelectProps> = ({ options, onChange, value }) => {
   return (
     <Wrapper onChange={(event) => onChange(event)} value={value}>
       {options.map((option) => (
-        <Option value={option.value} key={option.value}>
+        <option value={option.value} key={option.value}>
           {option.name}
-        </Option>
+        </option>
       ))}
     </Wrapper>
   )
